bf429661eb116efc2fc7e092518491be0317b1cf
kaijie26 <chenkaijie26@gmail.com>
2022-06-07 15:08:45 +0200

    js commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const generateButtom = document.getElementById('generate-ticket');
 generateButtom.addEventListener('click',
      function() {
+         // RACCOLTA DATI DEL UTENTE TRAMITE IL FORM
          const userName = document.getElementById('user-name').value;
          const userKm = parseInt(document.getElementById('user-km').value);
          const userAge = document.getElementById('user-age').value;
-         
+         // CALCOLO IL PREZZO STANDARD
          let priceKm = 0.21;
          const ticketPrice = userKm * priceKm;
 
+         // APPLICO LO SCONTO PER LE EVENTUALI FASCE D'ETÀ 
          let discount = 0;
 
          if (userAge === 'minorenne') {
@@ -19,6 +21,7 @@ generateButtom.addEventListener('click',
          const finalPrice = ticketPrice - discount;
          const ticketFinalPrice = finalPrice.toFixed(2);
 
+         // TICKET FINALE GENERATO DAL UTENTE
          document.getElementById('ticket-name').innerHTML = userName;
          let discountType = 'Biglietto Standard'
          if (userAge === 'minorenne') {
@@ -36,13 +39,15 @@ generateButtom.addEventListener('click',
      }
 
 )
-
+// RIMUOVO IL TICKET DOPO CHE L'UTENTE CLICCA 'ANNULLA'
 const cancelTicket = document.getElementById('cancel-ticket')
 cancelTicket.addEventListener('click',
     function() {
+        // Svuoto il form
         document.getElementById('user-name').value = ''
         document.getElementById('user-km').value = ''
         document.getElementById('user-age').value = 'maggiorenne'
+        // Rimuovo il ticket generato
         document.getElementById('ticket').classList.remove('active')
     }
 
